dc09caf726aea5ac4a16e15457aaf0e96107a928
Denys <denvoser@gmail.com>
2026-09-10 03:30:47 +0300

    feat: camera changed to first person

--- Source/project_SwordGame/Character/MainCharacter.cpp
+++ Source/project_SwordGame/Character/MainCharacter.cpp
@@ -7,33 +7,16 @@ AMainCharacter::AMainCharacter()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(GetRootComponent());
-	CameraBoom->TargetArmLength = 400.0f;
-	CameraBoom->bUsePawnControlRotation = true;
-
-	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
-	FollowCamera->bUsePawnControlRotation = false;
-
-
 }
 
 void AMainCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	bUseControllerRotationPitch = false;
-	bUseControllerRotationYaw = false;
-	bUseControllerRotationRoll = false;
-
 	GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.0f; 
 	GetCharacterMovement()->AirControl = 0.35f;    
 	GetCharacterMovement()->JumpZVelocity = 400.0f;
-
-	GetCharacterMovement()->bOrientRotationToMovement = true;
-	GetCharacterMovement()->RotationRate = FRotator(0.f, 360.f, 0.f);
 }
 void AMainCharacter::StartSprint()
 {
